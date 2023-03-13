59e7e446b982fbde57b13e5ec02186aa2df8bce3
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-03-13 10:42:20 +0530

    Contest 26-02-23

---
+++ Contest 26-02-23/Packing Rectangles (Contest).java
@@ -0,0 +1,45 @@
+/*
+Packing Rectangles (Contest)
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+There are n rectangles of the same size: w in width and h in length. It is required to find a square of the smallest size into which these rectangles can be packed. Rectangles cannot be rotated.
+Input
+The first line of the input contains three space-separated integers w, h and n.
+
+Constraints
+1 ≤ w, h, n ≤ 109
+Output
+Output the minimum length of a side of a square, into which the given rectangles can be packed.
+*/
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        long w=sc.nextLong();
+        long h=sc.nextLong();
+        long n=sc.nextLong();
+        long left=1, right=(long)(1e18)*1L;
+        long ans=-1;
+        while(left <= right){
+            long mid=left+(right-left)/2;
+            long sq=mid;
+            long x=sq/w;
+            long y=sq/h;
+            if(((y>0 && x>=((n-1)/y+1))||(x>0 && y>=((n-1)/x+1)))){
+            ans=sq;
+            right=mid-1;
+        }else{
+            left=mid+1;
+        }
+    }
+    System.out.print(ans);
+}
+}
---
+++ Contest 26-02-23/The high median paradigm (Contest).java
@@ -0,0 +1,40 @@
+/*
+The high median paradigm (Contest)
+easy
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+You are given two integer N and M. You have to construct an array A having N non- negative integers such that the sum of its elements is M. As there are many such arrays possible, find the maximum median of all such possible arrays.
+Input
+The first line of the input contains two integers N and M.
+
+Constraints:
+1 <= N, M <= 109
+Output
+Print the maximum median possible.
+*/
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    static int max(int n,int x){
+        if(n==1){
+            return x;
+        }else{
+            int m=(int)Math.ceil((double)(n)/(2.0));
+            m--;
+            int m1=x/(n-m);
+            return m1;
+        }
+    }
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        System.out.println(max(n,x));
+    }
+}
